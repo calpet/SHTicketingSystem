645fca2443a2e8f97ec4c89075b055e953edfb97
Calin Peters <calinpeters98@hotmail.nl>
2020-09-25 10:10:20 +0200

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SelfHelpTicketingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
