bdb9b1184d9d960981b5d5cf5b87002e85354d3e
Calin Peters <calinpeters98@hotmail.nl>
2020-09-25 19:11:04 +0200

    Extended DAL with methods for executing multiple queries at the same time.

--- SHTS_DAL/DatabaseConnection.cs
+++ SHTS_DAL/DatabaseConnection.cs
@@ -7,15 +7,19 @@ namespace SHTS_DAL
     public class DatabaseConnection : IDatabaseConnection
     {
         private static string conString = "Server=localhost;Database=shts;Username=root;Convert Zero Datetime=True;sslmode=none";
-        private MySqlConnection connection = new MySqlConnection(conString);
+        private MySqlConnection connection;
         private MySqlCommand command;
         private MySqlDataReader reader;
         private List<string> result;
 
-        private MySqlConnection OpenConnection()
+        public DatabaseConnection()
+        {
+            connection = new MySqlConnection(conString);
+        }
+
+        private void OpenConnection()
         {
             connection.Open();
-            return connection;
         }
 
         private void CloseConnection()
@@ -35,9 +39,8 @@ namespace SHTS_DAL
             result = new List<string>();
             command = connection.CreateCommand();
             command.CommandText = query;
-            /*
-             * Bind objects in the parameters array to every '@' in the query. 
-             */
+
+            //Bind objects in the parameters array to every '@' in the query. 
             var matches = Regex.Matches(query, @"[@#]\w+");
             for (int i = 0; i <= matches.Count; i++)
             {
@@ -59,6 +62,12 @@ namespace SHTS_DAL
             return result;
         }
 
+
+        /// <summary>
+        /// Method that's being used for queries that don't require getting values from the database, like INSERT queries.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="parameters"></param>
         public void ExecuteNonSearchQuery(string query, params object[] parameters)
         {
             OpenConnection();
@@ -71,6 +80,74 @@ namespace SHTS_DAL
             }
             command.ExecuteNonQuery();
             connection.Close();
+        }
+
+
+        /// <summary>
+        /// This method allows you to execute multiple queries at the same time.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="parameters"></param>
+        /// <returns>A list with arrays containing all the values found using the queries.</returns>
+        public List<string>[] MultipleSearchQueries(string[] queries, params object[][] parameters)
+        {
+            List<string>[] resultList = new List<string>[queries.Length];
+            OpenConnection();
+            MySqlCommand[] cmd = new MySqlCommand[queries.Length];
+            for (int i = 0; i < cmd.Length; i++)
+            {
+                reader = cmd[i].ExecuteReader();
+                resultList[i].Clear();
+
+                //Add parameters to every query
+                for (int j = 0; j < queries.Length; j++)
+                {
+                    var matches = Regex.Matches(queries[i], @"[@#]\w+");
+                    for (int k = 0; k < matches.Count; k++)
+                    {
+                        cmd[k].Parameters.AddWithValue(matches[k].Value, parameters[k]);
+                    }
+
+                    while (reader.Read())
+                    {
+                        for (int l = 0; l < reader.FieldCount; l++)
+                        {
+                            resultList[i].Add(reader[l].ToString());
+                        }
+                    }
+                }
+            }
+
+            CloseConnection();
+            return resultList;
+        }
+
+        /// <summary>
+        /// Executes multiple non-search queries like INSERT or DELETE at the same time. 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="parameters"></param>
+        public void MultipleNonSearchQueries(string[] query, params object[][] parameters)
+        {
+            OpenConnection();
+            MySqlCommand[] cmd = new MySqlCommand[query.Length];
+            for (int i = 0; i < cmd.Length; i++)
+            {
+                cmd[i] = new MySqlCommand();
+                cmd[i].CommandText = query[i];
+                for (int j = 0; j < query.Length; j++)
+                {
+                    var matches = Regex.Matches(query[i], @"[@#]\w+");
+                    for (int k = 0; k < matches.Count; k++)
+                    {
+                        cmd[i].Parameters.AddWithValue(matches[k].Value, parameters[k]);
+                    }
+                }
+
+                cmd[i].ExecuteNonQuery();
+            }
+
+            CloseConnection();
         }
     }
 }
--- SHTS_DAL/IDatabaseConnection.cs
+++ SHTS_DAL/IDatabaseConnection.cs
@@ -11,5 +11,7 @@ namespace SHTS_DAL
     {
         List<string> GetStringQuery(string query, params object[] parameters);
         void ExecuteNonSearchQuery(string query, params object[] parameters);
+        List<string>[] MultipleSearchQueries(string[] queries, params object[][] parameters);
+        void MultipleNonSearchQueries(string[] queries, params object[][] parameters);
     }
 }
