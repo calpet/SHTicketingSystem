3e70eb0b657e6e471cbd71c21fc6e05e56e2e332
Calin Peters <calinpeters98@hotmail.nl>
2020-09-30 22:50:13 +0200

    Added working Create & Delete methods to the UserRepository; Edit & Read TBA.

--- Shts.Dal/Repositories/UserRepository.cs
+++ Shts.Dal/Repositories/UserRepository.cs
@@ -7,6 +7,7 @@ namespace Shts.Dal
     {
         private IDatabaseConnection _dbCon;
         private object[] _params;
+        private List<string> _result;
 
         public UserRepository(IDatabaseConnection dbCon)
         {
@@ -21,24 +22,31 @@ namespace Shts.Dal
                 user.Password
             };
 
-            _dbCon.ExecuteNonSearchQuery($"INSERT INTO `person`(`email`, `password`) VALUES '@email', '@pwd'", _params);
+            _dbCon.ExecuteNonSearchQuery($"INSERT INTO `person`(`email`, `password`) VALUES (@email, @pwd)", _params);
         }
 
 
         //@TODO: Implement these methods.
-        public UserDto EditUser(UserDto user)
+        public void EditUser(UserDto user)
         {
-            return user;
+            _params = new object[] {user.FirstName, user.LastName, user.Email, user.Password, user.Email};
+            //_dbCon.ExecuteNonSearchQuery($"UPDATE `person` SET (`person`.`firstName` = @fname, `person`.`lastName` = @lname, `person`.`email` = @email, `person`.`password` = @pwd WHERE `person`.`email` = @email)", _params);
         }
 
         public void DeleteUser(int userId)
         {
-
+            _params = new object[] {userId};
+            _dbCon.ExecuteNonSearchQuery($"DELETE FROM `person` WHERE `person`.`personID` = @uid", _params);
         }
 
+
+        //@Todo: Fix this method pls:
         public UserDto GetUserById(int userId)
         {
-            return new UserDto();
+            _params = new object[] {userId};
+            //_result = _dbCon.GetStringQuery($"SELECT `email`, `password` FROM `person` WHERE `person`.`personID` = @uid", _params);
+            UserDto user = new UserDto() {Email = _result[0], Password = _result[1]};
+            return user;
         }
 
         public List<UserDto> GetAllUsers()
