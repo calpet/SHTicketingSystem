abd9fe12f2adfc90f6def840405798086a9081e5
Calin Peters <calinpeters98@hotmail.nl>
2020-10-05 22:46:34 +0200

    Added Enums to the Entities project.

---
+++ Shts_Entities/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shts_Entities.Enums
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ Shts_Entities/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shts_Entities.Enums
+{
+    public enum UserRole
+    {
+        SupportUser,
+        Agent,
+        Admin
+    }
+}
