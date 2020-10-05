df2f96aea8165275d5b92896de7b35a272f3e3b5
Calin Peters <calinpeters98@hotmail.nl>
2020-10-05 16:36:49 +0200

    Added Interfaces project for better separation of concerns.

---
+++ Shts_Interfaces/DAL/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shts_Interfaces
+{
+    public interface IRepository<T> where T: class
+    {
+        void Add(T entity);
+        void Edit(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+    }
+}
