73e571a5c0c88e2553887edd04be9f962dae38d2
Calin Peters <calinpeters98@hotmail.nl>
2020-10-06 15:47:58 +0200

    Removed useless from DAL

--- Shts.Dal/IDatabaseConnection.cs
+++ Shts.Dal/IDatabaseConnection.cs
@@ -6,7 +6,6 @@ namespace Shts.Dal
     {
         List<string> GetStringQuery(string query, params object[] parameters);
         void ExecuteNonSearchQuery(string query, params object[] parameters);
-        List<string>[] MultipleSearchQueries(string[] queries, params object[][] parameters);
-        void MultipleNonSearchQueries(string[] queries, params object[][] parameters);
+        
     }
 }
