ff86d38b9443be6f64cac37bc65e833574d74df1
Calin Peters <calinpeters98@hotmail.nl>
2020-10-06 12:22:39 +0200

    Fixed circular dependecy issue with BLL -> Factories -> DAL -> Factories -> BLL by removing BLL reference from DAL

---
+++ Shts_BusinessLogic/UserCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Shts.Dal.DTOs;
+using Shts_Factories;
+using Shts_Interfaces.BLL;
+
+namespace Shts_BusinessLogic
+{
+    public class UserCollection : IModelCollection<User>
+    {
+        private UserDto _dto;
+        public void Add(User user)
+        {
+            if (user != null)
+            {
+                _dto = new UserDto() { FirstName = user.FirstName, LastName = user.LastName, Email = user.Email, Password = user.Password};
+                DalFactory.UserRepo.Create(_dto);
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User SearchByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shts_Interfaces/BLL/IModelCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shts_Interfaces.BLL
+{
+    public interface IModelCollection<T> where T: class
+    {
+        void Add(T entity); 
+        List<T> GetAll();
+        T SearchByName(string name);
+
+    }
+}
