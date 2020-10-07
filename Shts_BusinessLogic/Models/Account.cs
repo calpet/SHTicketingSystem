d82324fd4da13911176745a0d67657c8431ad5f6
Calin Peters <calinpeters98@hotmail.nl>
2020-10-07 14:09:12 +0200

    Made some stuff more readable.

--- Shts_BusinessLogic/Models/Account.cs
+++ Shts_BusinessLogic/Models/Account.cs
@@ -6,9 +6,9 @@ namespace Shts_BusinessLogic.Models
 {
     public class Account
     {
-        public string Email { get; private set; }
-        public string Password { get; private set; }
-        public string ConfirmPassword { get; private set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public bool IsSignedIn { get; set; }
 
         private protected string EncryptPassword(string pwd)
