dc8e9e2d595eb9b735aa201f1c506c5de21b7213
Calin Peters <calinpeters98@hotmail.nl>
2020-10-07 14:16:11 +0200

    Added authentication controller.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfHelpTicketingSystem.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
