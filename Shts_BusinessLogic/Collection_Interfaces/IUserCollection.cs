6f8f3efa06e1dbae4bbf67053858a8407355cc25
Calin Peters <calinpeters98@hotmail.nl>
2020-10-16 15:38:30 +0200

    Removed useless interfaces, simplified the code structure.

--- Shts_BusinessLogic/Collection_Interfaces/IUserCollection.cs
+++ Shts_BusinessLogic/Collection_Interfaces/IUserCollection.cs
@@ -1,4 +1,4 @@
-﻿using Shts_BLLInterfaces;
+﻿using System.Collections.Generic;
 using Shts_Entities.Enums;
 
 namespace Shts_BusinessLogic
@@ -6,8 +6,10 @@ namespace Shts_BusinessLogic
 
    
     // Collection interfaces are put in the BusinessLogic so they can make use of the rich models & not cause circular dependencies in the Interface project :)
-    public interface IUserCollection : IModelCollection<User>
+    public interface IUserCollection
     {
+        void Add(User user);
+        List<User> GetAll();
         User SearchUserByFilter(UserFilter filter);
     }
 }
