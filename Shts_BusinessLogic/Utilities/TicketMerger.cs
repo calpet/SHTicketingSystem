f1631aa15bd275fdeb6c3070f2313c5891ff344d
Calin Peters <calinpeters98@hotmail.nl>
2020-11-23 13:45:01 +0100

    Refactored the TicketMerger in order to comply better with SOLID.

--- Shts_BusinessLogic/Utilities/TicketMerger.cs
+++ Shts_BusinessLogic/Utilities/TicketMerger.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Shts.Dal;
+using Shts.Dal.DTOs;
 using Shts_BusinessLogic.BusinessLogic_Interfaces;
 using Shts_BusinessLogic.Collection_Interfaces;
 using Shts_BusinessLogic.Collections;
-using Shts_BusinessLogic.Managers;
-using Shts_BusinessLogic.Models;
 using Shts_Entities.Enums;
 
 namespace Shts_BusinessLogic.Utilities
@@ -15,34 +11,39 @@ namespace Shts_BusinessLogic.Utilities
     public class TicketMerger
     {
         private static IUserCollection _userColl;
-        private static List<TicketDto> _tempList;
+        private static List<TicketDto> _ticketList;
 
         public static List<TicketDto> MergeDuplicateTickets(List<TicketDto> tickets)
         {
-            _tempList = new List<TicketDto>();
+            _ticketList = new List<TicketDto>();
             foreach (TicketDto t1 in tickets)
             {
                 bool duplicatesFound = false;
-                foreach (TicketDto t2 in _tempList)
+                foreach (TicketDto t2 in _ticketList)
                 {
                     if (t1.Subject == t2.Subject && t1.Content == t2.Content && t1.CreatedAt == t2.CreatedAt &&
                         t1.AuthorId != t2.AuthorId)
                     {
                         duplicatesFound = true;
-                        _userColl = new UserCollection(new AccountManager());
-                        var user = _userColl.GetUserById(t1.AuthorId);
-                        if (user.Role != UserRole.SupportUser)
-                        {
-                            t2.AgentId = user.Id;
-                        }
+                        SetTicketAgent(t2, t1);
                     }
                 }
 
                 if (!duplicatesFound)
-                    _tempList.Add(t1);
+                    _ticketList.Add(t1);
             }
 
-            return _tempList;
+            return _ticketList;
+        }
+
+        private static void SetTicketAgent(TicketDto realTicket, TicketDto agentTicket)
+        {
+            _userColl = new UserCollection(new AccountManager());
+            IUser user = _userColl.GetUserById(agentTicket.AuthorId);
+            if (user.Role != UserRole.SupportUser)
+            {
+                realTicket.AgentId = user.Id;
+            }
         }
     }
 }
