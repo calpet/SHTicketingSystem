79a7bebf7600908fe56edbb88ab008ccaeeb407a
Calin Peters <calinpeters98@hotmail.nl>
2020-12-04 15:14:53 +0100

    More model validation.

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -28,6 +28,7 @@ namespace SelfHelpTicketingSystem.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password required!")]
+        [StringLength(255, ErrorMessage = "Your password must be atleast 6 characters.", MinimumLength = 6)]
         [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{6,}$", ErrorMessage = "Passwords must be at least 6 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string Password { get; set; }
 
