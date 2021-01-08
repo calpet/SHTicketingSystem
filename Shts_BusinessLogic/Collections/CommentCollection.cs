c1298936bd0498d3cb85069598025e1cc11520d5
Calin Peters <calinpeters98@hotmail.nl>
2021-01-08 13:23:13 +0100

    Set up comment business logic.

--- Shts_BusinessLogic/Collections/CommentCollection.cs
+++ Shts_BusinessLogic/Collections/CommentCollection.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Shts_Interfaces.BusinessLogic;
 
 namespace Shts_BusinessLogic.Collections
 {
-    public class CommentCollection
+    public class CommentCollection : ICommentCollection
     {
+        public void Add(IComment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IComment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IComment> GetCommentsByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComment GetCommentById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Shts_BusinessLogic/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shts_Interfaces.BusinessLogic;
+
+namespace Shts_BusinessLogic.Models
+{
+    public class Comment : IComment
+    {
+        public int Id { get; set; }
+        public int CreatorId { get; set; }
+        public int TicketId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastEdited { get; set; }
+        public void Edit(IComment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IComment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
